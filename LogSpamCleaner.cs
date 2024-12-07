50621fd42b408be30a15f5035619bb318b64684e
esnya <2088693+esnya@users.noreply.github.com>
2024-12-07 17:05:18 +0900

    :tada: feat!: Initial Commit

---
+++ LogSpamCleaner.cs
@@ -0,0 +1,74 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using ResoniteModLoader;
+using System;
+using Elements.Core;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace LogSpamCleaner
+{
+    public class LogSpamCleaner : ResoniteMod
+    {
+        public override string Name => "LogSpamCleaner";
+        public override string Author => "esnya";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/esnya/ResoniteLogSpamCleaner";
+
+        private static ModConfiguration config;
+
+        [AutoRegisterConfigKey]
+        public static readonly ModConfigurationKey<bool> allowStackTraceOnError = new ModConfigurationKey<bool>("AllowStackTraceOnError", "Allow stack trace on error", () => true);
+        [AutoRegisterConfigKey]
+        public static readonly ModConfigurationKey<bool> allowStackTraceOnWarning = new ModConfigurationKey<bool>("AllowStackTraceOnWarning", "Allow stack trace on warning", () => false);
+        [AutoRegisterConfigKey]
+        public static readonly ModConfigurationKey<bool> allowStackTraceOnLog = new ModConfigurationKey<bool>("AllowStackTraceOnLog", "Allow stack trace on log", () => false);
+
+        public override void OnEngineInit()
+        {
+            config = GetConfiguration();
+            config.Save(true);
+
+            var harmony = new Harmony("com.nekometer.esnya.LogSpamCleaner");
+            harmony.PatchAll();
+        }
+
+        private static bool IsForbidden(string name)
+        {
+            switch (name)
+            {
+                case "Log":
+                    return !config.GetValue<bool>(allowStackTraceOnLog);
+                case "Warning":
+                    return !config.GetValue<bool>(allowStackTraceOnWarning);
+                case "Error":
+                    return !config.GetValue<bool>(allowStackTraceOnError);
+                default:
+                    return true;
+            }
+        }
+
+        [HarmonyPatch]
+        class PatchBase
+        {
+            static IEnumerable<MethodBase> TargetMethods()
+            {
+                var uniLogType = typeof(UniLog);
+                var args = new Type[] { typeof(string), typeof(bool) };
+
+                yield return uniLogType.GetMethod("Log", args);
+                yield return uniLogType.GetMethod("Warning", args);
+                yield return uniLogType.GetMethod("Error", args);
+            }
+
+            static void Prefix(MethodBase __originalMethod, string message, ref bool stackTrace)
+            {
+                if (stackTrace && IsForbidden(__originalMethod.Name))
+                {
+                    stackTrace = false;
+                }
+            }
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# LogSpamCleaner
+
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that disables stacktrace logs for specific levels.
+
+## Installation
+
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+1. Place [LogSpamCleaner.dll](https://github.com/esnya/ResoniteLogSpamCleaner/releases/latest/download/LogSpamCleaner.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create this folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
